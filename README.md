3ca692341dfe2e89294fce8dad6ce21c5c5ba455
vgmoose <me@vgmoose.com>
2022-08-30 14:28:42 -0400

    add switch-specific logic

---
+++ README.md
@@ -0,0 +1,5 @@
+## WiifiUShowMii
+Display active wifi info for the homebrew consoles.
+
+### License
+This app uses the Chesto library under [GPLv3](https://github.com/fortheusers/chesto/blob/master/LICENSE).
--- main.cpp
+++ main.cpp
@@ -2,20 +2,34 @@
 #include <switch.h>
 #endif
 
+#include <sstream>
+#include <iostream>
+
 #include "libs/chesto/src/Container.hpp"
 #include "libs/chesto/src/TextElement.hpp"
+#include "libs/chesto/src/Button.hpp"
 
 class WifiInfo {
+public:
     std::string ssid;
     std::string auth;
 };
 
-bool readWifiInfo() {
+bool readWifiInfo(WifiInfo* wifiInfo) {
 #ifdef SWITCH
     nifmInitialize(NifmServiceType_System);
-    NifmNetworkProfileData data;
-    Result res = nifmGetCurrentNetworkProfile(&data)
-    return true;
+    NifmNetworkProfileData profileData;
+    Result res = nifmGetCurrentNetworkProfile(&profileData);
+    if (R_SUCCEEDED(res)) {
+        auto data = profileData.wireless_setting_data;
+        char* ssid = data.ssid;
+        ssid[data.ssid_len] = '\0';
+        wifiInfo->ssid = std::string(ssid);
+        char* pass = (char*)data.passphrase; // TODO: unicode support?
+        pass[64] = '\0'; // 64 = 0x40, max password length
+        wifiInfo->auth = std::string(pass);
+        return true;
+    }
 #endif
     return false;
 }
@@ -24,16 +38,48 @@ int main(int argc, char* argv[])
 {
 	RootDisplay* display = new RootDisplay();
     display->backgroundColor = fromRGB(0x00, 0x88, 0xA3);
-    Container* con = new Container(COL_LAYOUT, 5);
+    Container* con = new Container(COL_LAYOUT, 8);
 
-    bool success = readWifiInfo();
+    WifiInfo wifiInfo;
+    bool success = readWifiInfo(&wifiInfo);
+    
+    // can be used to test on PC
+    // success = true;
+    // wifiInfo.ssid = "ssid_name";
+    // wifiInfo.auth = "password";
 
-    if (success) {
-        con->add(new TextElement("ssid: hello", 30));
-        con->add(new TextElement("auth: ********", 30));
-    } else {
-        con->add(new TextElement("Issue reading wifi info, make sure a main connection is established!", 30));
+    if (success)
+    {
+        Container* row1 = new Container(ROW_LAYOUT, 6);
+        row1->add(new TextElement("SSID:", 30));
+        row1->add(new TextElement(wifiInfo.ssid.c_str(), 30, 0, MONOSPACED))->y += 8;
+
+        Container* row2 = new Container(ROW_LAYOUT, 6);
+        row2->add(new TextElement("Auth:", 30));
+        row2->add(new Button("Show", X_BUTTON))->setAction([row1, row2, wifiInfo]() {
+            // replace button with the password
+            auto btn = row2->elements.back();
+            auto x = btn->x;
+            auto y = btn->y + 8;
+            row2->elements.pop_back();
+            row2->elements.push_back((new TextElement(wifiInfo.auth.c_str(), 30, 0, MONOSPACED))->setPosition(x, y));
+        });
+
+        con->add(row1);
+        con->add(row2);
+    } else
+    {
+        con->add(new TextElement("There was an issue trying to read WiFi info!", 36));
+        con->add(new TextElement("This app can only display info about the current network.", 24))->centerHorizontallyIn(con);
+        con->add(new TextElement("Please connect to a wireless hotspot in settings and retry.", 24))->centerHorizontallyIn(con);
     }
+
+    con->height += 10;
+
+    con->add(new Button("Close App", B_BUTTON))->centerHorizontallyIn(con)->setAction([display](){
+		display->exitRequested = true;
+		display->isRunning = false;
+	});
 
     con->centerIn(display);
     display->child(con);
